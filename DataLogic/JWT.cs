945b0d76b3128ca3111d69defa6f4966f2dfc624
SarvashAgnointel <sarvash@agnointel.ai>
2025-02-12 18:38:19 +0530

    initialized by sarvash on 12-02-2025 for basith

---
+++ DataLogic/JWT.cs
@@ -0,0 +1,39 @@
+﻿using AgnoCon.Controllers;
+using DBAccess;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+
+namespace TravelAd_Api.DataLogic
+{
+    public class JWT
+    {
+        private readonly IConfiguration _configuration;
+        private readonly IDbHandler _dbHandler;
+        private readonly string fromEmail;
+        private readonly ILogger<AuthenticationController> _logger;
+
+        public JWT(IConfiguration configuration, IDbHandler dbHandler, ILogger<AuthenticationController> logger)
+        {
+            _configuration = configuration;
+            _dbHandler = dbHandler;
+            fromEmail = _configuration.GetValue<string>("Emailid");
+            _logger = logger;
+        }
+
+        public string GenerateToken()
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer: _configuration["Jwt:Issuer"],
+                audience: _configuration["Jwt:Audience"],
+                expires: DateTime.Now.AddMinutes(30),
+                signingCredentials: credentials
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
---
+++ Models/AdminModel.cs
@@ -0,0 +1,12 @@
+﻿namespace TravelAd_Api.Models
+{
+    public class AdminModel
+    {
+        public class UpdateCampaign
+        {
+            public int campaignId { get; set; }
+            public string status { get; set; }
+            public int serverId { get; set; }
+        }
+    }
+}
